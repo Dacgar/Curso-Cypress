196378553e25fdee06247d3e0e415ae6e378b06f
Dacgar <diecadman@hotmail.com>
2024-09-08 15:49:58 -0500

    Fix report and flakyness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random05';
+    const contraseña = 'random05'
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(usuario,contraseña)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    cy.wait(5000)
   })
 })
